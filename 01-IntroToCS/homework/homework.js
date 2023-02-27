09333a095c24152793ca8f6dba8b9a57fce8b790
Melissa Vela <melissa@instarent.com.au>
2023-02-27 22:19:07 +1100

    first commit

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,28 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let result=0;
+   let i=0;
+   let  j=num.length-1;
+   
 
-function DecimalABinario(num) {}
+   for(i=0;i<num.length; i++){//i 1   1<=3
+      result = result +  num[i]*Math.pow(2,j);
+      j--;
+   }
+   console.log(result)
+   return result;
+}
+
+function DecimalABinario(num) {
+      var arr = [];
+      for (var i = 0; num > 0; i++){
+         arr[i] = num %2;
+         num = Math.floor(num/2);
+      };
+     
+      return arr.reverse().join('');
+}
 
 module.exports = {
    BinarioADecimal,
